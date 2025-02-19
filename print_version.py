51ed78fae4569384ee6187ec6c953f39b6abd6ae
oleovarsh <varshini@mymail.sutd.edu.sg>
2025-02-19 23:23:19 +0800

    new version

--- print_version.py
+++ print_version.py
@@ -2,4 +2,4 @@ import git
 repo = git.Repo("C:\\Users\\varsh\\Documents\\hivebox")
 tags = repo.git.ls_remote("--tags", "origin")
 
-print(tags.split('\n')[0].split('/')[-1])
+print(tags.split('\n')[-1].split('/')[-1])
