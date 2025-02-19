1dadf436620f6cd578eef83688fbc41afc150a0e
oleovarsh <varshini@mymail.sutd.edu.sg>
2025-02-19 23:22:07 +0800

    print tag

--- print_version.py
+++ print_version.py
@@ -1,2 +1,5 @@
-print("Hello world")
-print("hello again")
+import git 
+repo = git.Repo("C:\\Users\\varsh\\Documents\\hivebox")
+tags = repo.git.ls_remote("--tags", "origin")
+
+print(tags.split('\n')[0].split('/')[-1])
